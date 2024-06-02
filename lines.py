e92950e154d6131121b0fe29b2e94d0c26552565
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-02 16:46:42 -0600

    Check that command line arguments == 2

--- lines.py
+++ lines.py
@@ -1,7 +1,10 @@
-import sys
+from sys import argv, exit
 
 def main():
-    ...
+    if len(argv) < 2:
+        exit('Too few command_line arguments')
+    elif len(argv) > 2:
+        exit('Too many command_line arguments')
 
 
 if __name__ == '__main__':
