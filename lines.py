f1fcbd425d8b88a8dec74af572f5fe260e214d3d
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-02 17:16:24 -0600

    Clean up code

--- lines.py
+++ lines.py
@@ -1,13 +1,14 @@
 from sys import argv, exit
 
+
 def main():
     if len(argv) < 2:
-        exit('Too few command_line arguments')
+        exit("Too few command_line arguments")
     elif len(argv) > 2:
-        exit('Too many command_line arguments')
+        exit("Too many command_line arguments")
     else:
-        if not argv[1][-3] != '.py':
-            exit('Not a python file')
+        if not argv[1][-3] != ".py":
+            exit("Not a python file")
         else:
             print(count(argv[1]))
 
@@ -15,14 +16,14 @@ def main():
 def count(file):
     try:
         lines = 0
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             for line in f:
-                if not (line.strip().startswith('#') or line.strip() == ''):
+                if not (line.strip().startswith("#") or line.strip() == ""):
                     lines += 1
         return lines
     except FileNotFoundError:
-        exit('File does not exist')
+        exit("File does not exist")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
